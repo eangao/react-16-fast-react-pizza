0bc1517be7af3845a16fe5ac5d0e1cb23b867087
elmar <elmarcera@gmail.com>
2024-03-07 12:39:56 +0800

    Setting Up a New Project: Fast React Pizza Co.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+//create react project using vite
+//npm create vite@latest  -> for future project
+//npm create vite@4 -> to match in training
+
+///////////////
+// install eslint
+//npm i eslint vite-plugin-eslint eslint-config-react-app --save-dev
