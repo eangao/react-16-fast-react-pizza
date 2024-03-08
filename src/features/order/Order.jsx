9b52bc418f542219e61d2cceebbd4b15120b5993
elmar <elmarcera@gmail.com>
2024-03-08 12:06:27 +0800

    Fetching Orders

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,47 +1,16 @@
 // Test ID: IIDSAT
 
+import { useLoaderData } from "react-router-dom";
+import { getOrder } from "../../services/apiRestaurant";
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
 } from "../../utils/helpers";
 
-const order = {
-  id: "ABCDEF",
-  customer: "Jonas",
-  phone: "123456789",
-  address: "Arroios, Lisbon , Portugal",
-  priority: true,
-  estimatedDelivery: "2027-04-25T10:00:00",
-  cart: [
-    {
-      pizzaId: 7,
-      name: "Napoli",
-      quantity: 3,
-      unitPrice: 16,
-      totalPrice: 48,
-    },
-    {
-      pizzaId: 5,
-      name: "Diavola",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-    {
-      pizzaId: 3,
-      name: "Romana",
-      quantity: 1,
-      unitPrice: 15,
-      totalPrice: 15,
-    },
-  ],
-  position: "-9.000,38.000",
-  orderPrice: 95,
-  priorityPrice: 19,
-};
-
 function Order() {
+  const order = useLoaderData();
+
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -81,6 +50,33 @@ function Order() {
       </div>
     </div>
   );
+}
+
+/////////////////////
+// So we do await getOrder, but now remember
+// that this needs to receive the ID.
+// So, how do we get the ID from the
+// URL right into this function?
+// Well, we previously used the useParams hook, right?
+// So something like this. However, since this is a hook
+// it only works inside components.
+// It doesn't work in regular functions.
+
+// But, luckily for us, React Router has, of course,
+// thought of the situation, and therefore it passes
+// in some data into the loader function as it calls it,
+// and one of the properties of the object that
+// the loader function receives is exactly the params.
+// So, we de-structure that object here
+// and get out the params.
+// And so, here, we then get params.orderId
+// and it is called orderId because that is
+// exactly the name that we gave it here.
+// { path: "/order/:orderId", element: <Order /> },
+export async function loader({ params }) {
+  const order = await getOrder(params.orderId);
+
+  return order;
 }
 
 export default Order;
