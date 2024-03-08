16735ed18dc839c77e81625b0095e4ef79d56dcd
elmar <elmarcera@gmail.com>
2024-03-08 11:14:16 +0800

    Handling Errors With Error Elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import Home from "./ui/Home";
+import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
@@ -9,9 +11,29 @@ import AppLayout from "./ui/AppLayout";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/menu", element: <Menu />, loader: menuLoader },
+
+      //       However, now we see that this error message
+      // is really completely outside of our layout.
+      // So the complete application has been replaced
+      // with just this screen here
+      // and so that's really not what we want.
+      // Instead, in this situation,
+      // we want the error to appear within the layout
+      // and so we can basically also place the same error element
+      // inside of any of these child routes.
+      // Now in this case,
+      // it only makes sense to place it here because this component
+      // is the only one that is loading some data.
+      // So only here things can actually go wrong
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
       { path: "/order/:orderId", element: <Order /> },
